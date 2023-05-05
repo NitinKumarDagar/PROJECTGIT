a6311f91c53677cccdf31bd248ee818aa4de31f1
NitinKumarDagar <129051312+NitinKumarDagar@users.noreply.github.com>
2023-05-05 21:38:38 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
          System.out.println("Hello, Nitin!"); 
+        System.out.println("Hello, Nitin! I have made some changes on GitHUB"); 
+        
     }
 }
